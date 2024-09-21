8a7fb0ab3104492e1696b85de533423a05327696
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-09-21 18:25:28 +0900

    이메일 주소 인증 구현

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,37 +3,70 @@
   <div class="sign-up-form">
     <h2>회원 가입</h2>
     <form @submit.prevent="submitForm">
-      <div>
-        <label for="email">Username</label>
-        <input type="email" v-model="username" id="username" required />
+      <div id="username-form">
+        <input type="email" v-model="username" placeholder="이메일을 입력하세요" required />
+        <button id="sendEmailAuth" @click="sendEmailAuth">인증메일 발송하기</button>
+        <input type="text" v-model="secretCode" v-if="isCodeSent" placeholder="인증코드를 입력하세요" required />
+        <button v-if="isCodeSent" @click="verifyEmailAuth">인증하기</button>
       </div>
 
-      <div>
-        <label for="password">Password</label>
-        <input type="password" v-model="password" id="password" required />
+      <div id="password-form">
+        <input type="password" v-model="password" id="password" placeholder="비밀번호를 입력하세요" required />
+        <input type="password" v-model="passwordConfirm" id="password_confirm" placeholder="비밀번호를 한 번 더 입력하세요" required />
       </div>
 
-      <button type="submit">Sign Up</button>
+      <button onsubmit="submitSignup()">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import router from "../routes";
+import router from '@/routes';
+import {API_URL} from "@/constants";
 
 export default {
   name: 'SignupForm',
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      secretCode: '',
+      isCodeSent: false,
     };
   },
   methods: {
-    submitForm() {
+    verifyEmailAuth() {
+      axios.get(`${API_URL}/user/verify-email-auth?secretCode=${this.secretCode}`)
+          .then(response => {
+            console.log(response);
+            if (response.data) {
+              alert("가입할 수 있는 이메일입니다.");
+            } else {
+              alert("인증코드가 불일치합니다.");
+            }
+          }).catch((err)=>{
+            console.error(err);
+            alert("이메일 인증에 실패했습니다.")
+      });
+    },
+    sendEmailAuth() {
+      axios.post(`${API_URL}/user/send-email-auth?recipient=${this.username}`)
+          .then(() => {
+            alert("인증코드를 발송하였습니다.");
+            this.isCodeSent = true;
+          }).catch((err)=>{
+            console.error("failed to sent auth", err.response);
+            if (err?.response?.status === 403 && err?.response?.data.message === 'EMAIL DUPLICATED') {
+              alert("이미 가입된 이메일입니다.");
+            }
+            console.error(err);
+            alert("이메일 인증 코드 발송에 실패하였습니다.");
+          });
+    },
+    submitSignup() {
       // 서버에 이메일 중복 여부 확인
-      axios.get(`http://localhost:8080/api/user/check-username-available`, {
+      axios.get(`${API_URL}/user/verify-username-available`, {
         params: { username: this.username }
       })
           .then((response) => {
@@ -44,7 +77,7 @@ export default {
             } else {
               console.debug("available to signup: ", this.username);
               // call signup api
-              this.signup(this.username, this.password);
+              this.signupApi(this.username, this.password);
             }
           })
           .catch((err) => {
@@ -52,9 +85,9 @@ export default {
             alert("이메일 중복여부 확인 중 알 수 없는 에러가 발생하였습니다.");
           });
     },
-    signup(username, password) {
-      axios.post(`http://localhost:8080/api/user/create`, {
-        username: username, password: password
+    signupApi(username, password) {
+      axios.post(`${API_URL}/api/user/create`, {
+        params: { username: username, password: password }
       })
           .then((response) => {
             console.debug(response);
@@ -87,6 +120,7 @@ label {
 input {
   width: 100%;
   padding: 0.5rem;
+  margin-top: 10px;
   margin-bottom: 10px;
   border: 1px solid #ccc;
   border-radius: 3px;
@@ -100,6 +134,10 @@ button {
   border: none;
   border-radius: 3px;
   cursor: pointer;
+}
+
+#username-form {
+  margin-bottom: 1rem;
 }
 
 button:hover {
