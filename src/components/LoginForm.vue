7a240c74fe8e461c8de6954f08a46fccc7af92d9
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-09-21 18:25:28 +0900

    이메일 주소 인증 구현

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,13 +4,11 @@
     <h2>로그인</h2>
     <form @submit.prevent="submitForm">
       <div>
-        <label for="email">Email</label>
-        <input type="email" v-model="email" id="email" required />
+        <input type="email" v-model="username" id="username" placeholder="이메일을 입력하세요" required />
       </div>
 
       <div>
-        <label for="password">Password</label>
-        <input type="password" v-model="password" id="password" required />
+        <input type="password" v-model="password" id="password" placeholder="비밀번호를 입력하세요" required />
       </div>
 
       <button type="submit">Login</button>
@@ -19,6 +17,9 @@
 </template>
 
 <script>
+import axios from 'axios';
+import router from '@/routes';
+
 export default {
   name: 'LoginForm',
   data() {
@@ -29,13 +30,22 @@ export default {
   },
   methods: {
     submitForm() {
-      // 간단한 로그인 폼 제출 로직
-      console.log('Login Form Submitted');
-      console.log('Email:', this.email);
-      console.log('Password:', this.password);
-
-      // 서버에 로그인 API 요청을 보내는 부분
-      alert(`Login Successful!\nEmail: ${this.email}`);
+      axios.post(`http://localhost:8080/api/user/login`, {
+        username: this.username, password: this.password
+      })
+          .then(() => {
+            console.debug('login success');
+            alert("환영합니다!");
+            return router.push('/');
+          })
+          .catch((err) => {
+            if (err.response.status === 401) {
+              alert("이메일 또는 비밀번호가 정확하지 않습니다.")
+              return;
+            }
+            console.error('login failed', err);
+            alert("로그인에 실패하였습니다.")
+          });
     }
   }
 };
