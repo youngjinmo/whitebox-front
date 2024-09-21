4789497228df54ee6c01d31011e71fd31331bf76
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-09-22 00:11:36 +0900

    회원가입 리팩토링 및 마이너 기능 추가 (비밀번호 확인)

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -4,18 +4,17 @@
     <h2>회원 가입</h2>
     <form @submit.prevent="submitForm">
       <div id="username-form">
-        <input type="email" v-model="username" placeholder="이메일을 입력하세요" required />
+        <input type="email" v-model="username" placeholder="이메일을 입력하세요" :disabled="isVerifiedEmail" required />
         <button id="sendEmailAuth" @click="sendEmailAuth">인증메일 발송하기</button>
         <input type="text" v-model="secretCode" v-if="isCodeSent" placeholder="인증코드를 입력하세요" required />
         <button v-if="isCodeSent" @click="verifyEmailAuth">인증하기</button>
       </div>
 
       <div id="password-form">
-        <input type="password" v-model="password" id="password" placeholder="비밀번호를 입력하세요" required />
-        <input type="password" v-model="passwordConfirm" id="password_confirm" placeholder="비밀번호를 한 번 더 입력하세요" required />
+        <input type="password" v-model="password" placeholder="비밀번호를 입력하세요" required/>
+        <input type="password" v-model="confirmPassword" @blur="isConfirmedPassword" placeholder="비밀번호를 한 번 더 입력하세요" required />
       </div>
-
-      <button onsubmit="submitSignup()">Submit</button>
+      <button @click="submitSignup">가입하기</button>
     </form>
   </div>
 </template>
@@ -23,24 +22,41 @@
 <script>
 import axios from 'axios';
 import router from '@/routes';
-import {API_URL} from "@/constants";
+import API_URL from "@/constants";
 
 export default {
   name: 'SignupForm',
   data() {
     return {
       username: '',
-      password: '',
-      secretCode: '',
+      isVerifiedEmail: false,
       isCodeSent: false,
+      secretCode: '',
+      password: '',
+      confirmPassword: '',
+      matchPassword: false
     };
   },
   methods: {
+    isConfirmedPassword() {
+      if (this.password.length < 8) {
+        alert("비밀번호는 8자리 이상 입력해주세요.");
+        return false;
+      }
+      if (this.password != '' && this.password === this.confirmPassword) {
+        this.matchPassword = true;
+        return true;
+      } else {
+        this.matchPassword = false;
+        alert("비밀번호가 불일치합니다.");
+        return false;
+      }
+    },
     verifyEmailAuth() {
       axios.get(`${API_URL}/user/verify-email-auth?secretCode=${this.secretCode}`)
           .then(response => {
-            console.log(response);
             if (response.data) {
+              this.isVerifiedEmail = true;
               alert("가입할 수 있는 이메일입니다.");
             } else {
               alert("인증코드가 불일치합니다.");
@@ -53,57 +69,52 @@ export default {
     sendEmailAuth() {
       axios.post(`${API_URL}/user/send-email-auth?recipient=${this.username}`)
           .then(() => {
-            alert("인증코드를 발송하였습니다.");
             this.isCodeSent = true;
+            alert("인증코드를 발송하였습니다.");
           }).catch((err)=>{
-            console.error("failed to sent auth", err.response);
             if (err?.response?.status === 403 && err?.response?.data.message === 'EMAIL DUPLICATED') {
               alert("이미 가입된 이메일입니다.");
+              return;
             }
             console.error(err);
             alert("이메일 인증 코드 발송에 실패하였습니다.");
           });
     },
     submitSignup() {
-      // 서버에 이메일 중복 여부 확인
-      axios.get(`${API_URL}/user/verify-username-available`, {
-        params: { username: this.username }
+      if (!this.isVerifiedEmail) {
+        alert("이메일 인증이 필요합니다.");
+        return;
+      }
+      if (!this.matchPassword) {
+        alert("비밀번호가 일치하지 않습니다.");
+        return;
+      }
+      axios.post(`${API_URL}/user/create`, {
+        username: this.username, password: this.password
       })
-          .then((response) => {
-            if (response.data) {
-              alert(`${this.username}은 이미 사용중인 이메일입니다. 로그인을 해주세요.`);
-              // redirect login component
-              return router.push('/login');
-            } else {
-              console.debug("available to signup: ", this.username);
-              // call signup api
-              this.signupApi(this.username, this.password);
-            }
-          })
-          .catch((err) => {
-            console.error("failed to check is username duplicated", err);
-            alert("이메일 중복여부 확인 중 알 수 없는 에러가 발생하였습니다.");
-          });
-    },
-    signupApi(username, password) {
-      axios.post(`${API_URL}/api/user/create`, {
-        params: { username: username, password: password }
-      })
-          .then((response) => {
-            console.debug(response);
+          .then(() => {
             alert("환영합니다!");
-            return router.push('/');
+            return router.push('/login');
           })
           .catch((err) => {
             console.error("failed to create account", err);
             alert("회원가입 도중 에러가 발생하였습니다.");
           });
-    }
+    },
   }
 };
 </script>
 
 <style scoped>
+.match {
+  border-color: blue;
+  color: blue;
+}
+
+.mismatch {
+  color: red;
+}
+
 .sign-up-form {
   max-width: 300px;
   margin: 0 auto;
