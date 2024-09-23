bf6228465bd0b885dcd750f5359220861a312a0b
Youngjin Mo <dev.youngjinmo@gmail.com>
2024-09-23 16:42:34 +0900

    Modal 컴포넌트 적용

---
+++ src/components/Modal.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-if="isVisible" class="modal-overlay">
+    <div class="modal-content">
+      <h3>{{ title }}</h3>
+      <p>{{ message }}</p>
+      <button @click="closeModal">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalComponent',
+  props: {
+    isVisible: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: '알림'
+    },
+    message: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+</style>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="sign-up-form">
     <h2>회원 가입</h2>
-    <form @submit.prevent="submitForm">
+    <form @submit.prevent="submitSignup">
       <div id="username-form">
         <input type="email" v-model="username" placeholder="이메일을 입력하세요" :disabled="isVerifiedEmail" required />
         <button id="sendEmailAuth" @click="sendEmailAuth">인증메일 발송하기</button>
@@ -12,10 +12,16 @@
 
       <div id="password-form">
         <input type="password" v-model="password" placeholder="비밀번호를 입력하세요" required/>
-        <input type="password" v-model="confirmPassword" @blur="isConfirmedPassword" placeholder="비밀번호를 한 번 더 입력하세요" required />
+        <input type="password" v-model="confirmPassword" placeholder="비밀번호를 한 번 더 입력하세요" required />
       </div>
       <button @click="submitSignup">가입하기</button>
     </form>
+    <ModalComponent
+        :is-visible="modalVisible"
+        :title="modalTitle"
+        :message="modalMessage"
+        @close="modalVisible = false"
+    />
   </div>
 </template>
 
@@ -23,9 +29,13 @@
 import axios from 'axios';
 import router from '@/routes';
 import { API_URL } from "@/constants";
+import ModalComponent from "@/components/Modal.vue";
 
 export default {
   name: 'SignupForm',
+  components: {
+    ModalComponent
+  },
   data() {
     return {
       username: '',
@@ -34,13 +44,17 @@ export default {
       secretCode: '',
       password: '',
       confirmPassword: '',
-      matchPassword: false
+      matchPassword: false,
+      // modal
+      modalVisible: false,
+      modalTitle: "",
+      modalMessage: "",
     };
   },
   methods: {
-    isConfirmedPassword() {
+    isValidPassword() {
       if (this.password.length < 8) {
-        alert("비밀번호는 8자리 이상 입력해주세요.");
+        this.showModal("회원가입 오류", "비밀번호는 8자리 이상 입력해주세요.");
         return false;
       }
       if (this.password != '' && this.password === this.confirmPassword) {
@@ -48,7 +62,7 @@ export default {
         return true;
       } else {
         this.matchPassword = false;
-        alert("비밀번호가 불일치합니다.");
+        this.showModal("회원가입 오류", "비밀번호가 불일치합니다.");
         return false;
       }
     },
@@ -57,49 +71,53 @@ export default {
           .then(response => {
             if (response.data) {
               this.isVerifiedEmail = true;
-              alert("가입할 수 있는 이메일입니다.");
+              this.showModal("회원가입 오류", "가입할 수 있는 이메일입니다.");
             } else {
-              alert("인증코드가 불일치합니다.");
+              this.showModal("회원가입 오류", "인증코드가 불일치합니다.");
             }
           }).catch((err)=>{
             console.error(err);
-            alert("이메일 인증에 실패했습니다.")
+            this.showModal("회원가입 오류", "이메일 인증에 실패했습니다.")
       });
     },
     sendEmailAuth() {
       axios.post(`${API_URL}/user/send-email-auth?recipient=${this.username}`)
           .then(() => {
             this.isCodeSent = true;
-            alert("인증코드를 발송하였습니다.");
+            this.showModal(null, "인증코드를 발송하였습니다.");
           }).catch((err)=>{
             if (err?.response?.status === 403 && err?.response?.data.message === 'EMAIL DUPLICATED') {
-              alert("이미 가입된 이메일입니다.");
+              this.showModal("회원가입 오류", "이미 가입된 이메일입니다.");
               return;
             }
             console.error(err);
-            alert("이메일 인증 코드 발송에 실패하였습니다.");
+            this.showModal("회원가입 오류", "이메일 인증 코드 발송에 실패하였습니다.");
           });
     },
     submitSignup() {
       if (!this.isVerifiedEmail) {
-        alert("이메일 인증이 필요합니다.");
+        this.showModal("회원가입 오류", "이메일 인증이 필요합니다.");
         return;
       }
-      if (!this.matchPassword) {
-        alert("비밀번호가 일치하지 않습니다.");
+      if (!this.isValidPassword()) {
         return;
       }
       axios.post(`${API_URL}/user/create`, {
         username: this.username, password: this.password
       })
           .then(() => {
-            alert("환영합니다!");
+            this.showModal("환영합니다!");
             return router.push('/login');
           })
           .catch((err) => {
             console.error("failed to create account", err);
-            alert("회원가입 도중 에러가 발생하였습니다.");
+            this.showModal("회원가입 도중 에러가 발생하였습니다.");
           });
+    },
+    showModal(title, message) {
+      this.modalTitle = title;
+      this.modalMessage = message;
+      this.modalVisible = true;
     },
   }
 };
